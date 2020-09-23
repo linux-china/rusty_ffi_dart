e839449c59f59390ba96a4972b9beb417a50b4cb
linux_china <libing.chen@gmail.com>
2020-09-23 10:23:22 -0700

    add comments

--- ffi-demo.php
+++ ffi-demo.php
@@ -1,5 +1,5 @@
 <?php
-
+// php.ini configuration:  [ffi] ffi.enable=true
 $file='target/debug/libplay_once.dylib'; // the path of our dll
 $ffi=FFI::cdef('const char* play_once(const char *pong);',$file);
 
